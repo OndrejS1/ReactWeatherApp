8abb66ba6f2c3d96156dac7d4958696e65f22dc4
OndrejS1 <svojse.o@gmail.com>
2016-06-26 17:34:20 +0200

    filling About section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,10 @@ var React = require("react");
 var About = (props) => {
 
 	return (
+
 		<div>
-			<h3> About component </h3>
+			<h1> About </h1>
+			<p> Simple Weather app made for practice porpuses using 3rd party API from OpenWeather.org. </p>
 			<p> This is Weather App devoloped by Ondřej Svojše </p>
 		</div>
 			);
