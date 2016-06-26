d10bfaad6b666c562569b15569b6dec7cad985ea
OndrejS1 <svojse.o@gmail.com>
2016-06-26 17:44:17 +0200

    Small Changes

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -6,9 +6,23 @@ var About = (props) => {
 	return (
 
 		<div>
-			<h1> About </h1>
+			<h1 className="text-center"> About </h1>
 			<p> Simple Weather app made for practice porpuses using 3rd party API from OpenWeather.org. </p>
 			<p> This is Weather App devoloped by Ondřej Svojše </p>
+			<ol>
+				<li>
+
+					<a href="https://facebook.github.io/react"> React </a> - This was the Javascript framework used.
+
+				</li>
+
+				<li>
+
+					<a href="http://openweathermap.org"> Open Weather Map </a> - Third party API providing weather datas.
+
+				</li>
+
+			</ol>
 		</div>
 			);
 
